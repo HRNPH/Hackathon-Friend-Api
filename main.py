205cf5832b3a934b54863ca5f1abebe15f272b67
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-06-26 04:07:39 +0700

    Another routes

--- main.py
+++ main.py
@@ -84,3 +84,52 @@ async def delete_userpost(id: str):
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"userpost {id} not found")
+
+# camp posts API
+# class camppost(BaseModel):
+#     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+#     title: str = Field(...)
+#     detail: str = Field(...)
+
+#     class Config:
+#         allow_population_by_field_name = True
+#         arbitrary_types_allowed = True
+#         json_encoders = {ObjectId: str}
+#         schema_extra = {
+#             "example": {
+#                 "name": "ตัวตึง",
+#                 "detail": "หาตัวตึงกว่านี้ไม่มีแล้ว ผมนี่แหละของจริง 200%",
+#             }
+#         }
+
+
+
+# @app.post("/camps", response_description="Add new camppost", response_model=camppost)
+# async def create_camppost(camppost: camppost = Body(...)):
+#     camppost = jsonable_encoder(camppost)
+#     new_camppost = await db["campposts"].insert_one(camppost)
+#     created_camppost = await db["campposts"].find_one({"_id": new_camppost.inserted_id})
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_camppost)
+
+
+# @app.get("/camps", response_description="List all campposts", response_model=List[camppost])
+# async def list_campposts():
+#     campposts = await db["campposts"].find().to_list(1000)
+#     return campposts
+
+
+# @app.get("/camps/{id}", response_description="Get a single camppost", response_model=camppost)
+# async def show_camppost(id: str):
+#     if (camppost := await db["campposts"].find_one({"_id": id})) is not None:
+#         return camppost
+
+#     raise HTTPException(status_code=404, detail=f"camppost {id} not found")
+
+# @app.delete("/camps/{id}", response_description="Delete a camppost")
+# async def delete_camppost(id: str):
+#     delete_result = await db["campposts"].delete_one({"_id": id})
+
+#     if delete_result.deleted_count == 1:
+#         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+
+#     raise HTTPException(status_code=404, detail=f"camppost {id} not found")
