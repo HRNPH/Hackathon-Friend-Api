369bc5e7d93107a48da5477f0e666736573a49c5
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-06-25 23:44:56 +0700

    update

--- main.py
+++ main.py
@@ -28,48 +28,20 @@ class PyObjectId(ObjectId):
         field_schema.update(type="string")
 
 
-class StudentModel(BaseModel):
+class userpost(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str = Field(...)
-    email: EmailStr = Field(...)
-    course: str = Field(...)
-    gpa: float = Field(..., le=4.0)
+    detail: EmailStr = Field(...)
 
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-                "course": "Experiments, Science, and Fashion in Nanophotonics",
-                "gpa": "3.0",
-            }
-        }
 
 
-class UpdateStudentModel(BaseModel):
-    name: Optional[str]
-    email: Optional[EmailStr]
-    course: Optional[str]
-    gpa: Optional[float]
 
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-                "course": "Experiments, Science, and Fashion in Nanophotonics",
-                "gpa": "3.0",
-            }
-        }
-
-
-@app.post("/", response_description="Add new student", response_model=StudentModel)
-async def create_student(student: StudentModel = Body(...)):
+@app.post("/", response_description="Add new student", response_model=userpost)
+async def create_student(student: userpost = Body(...)):
     student = jsonable_encoder(student)
     new_student = await db["students"].insert_one(student)
     created_student = await db["students"].find_one({"_id": new_student.inserted_id})
@@ -77,7 +49,7 @@ async def create_student(student: StudentModel = Body(...)):
 
 
 @app.get(
-    "/", response_description="List all students", response_model=List[StudentModel]
+    "/", response_description="List all students", response_model=List[userpost]
 )
 async def list_students():
     students = await db["students"].find().to_list(1000)
@@ -85,33 +57,13 @@ async def list_students():
 
 
 @app.get(
-    "/{id}", response_description="Get a single student", response_model=StudentModel
+    "/{id}", response_description="Get a single student", response_model=userpost
 )
 async def show_student(id: str):
     if (student := await db["students"].find_one({"_id": id})) is not None:
         return student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
-
-
-@app.put("/{id}", response_description="Update a student", response_model=StudentModel)
-async def update_student(id: str, student: UpdateStudentModel = Body(...)):
-    student = {k: v for k, v in student.dict().items() if v is not None}
-
-    if len(student) >= 1:
-        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
-
-        if update_result.modified_count == 1:
-            if (
-                updated_student := await db["students"].find_one({"_id": id})
-            ) is not None:
-                return updated_student
-
-    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
-        return existing_student
-
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
-
 
 @app.delete("/{id}", response_description="Delete a student")
 async def delete_student(id: str):
@@ -121,4 +73,3 @@ async def delete_student(id: str):
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
-    
